0d8de9dbce58f292ce37c70f0ec323958337f5f7
PiyushGoyal02 <goyalp3542@gmail.com>
2024-06-14 22:31:24 +0530

    /api add

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send({message: "Hey I am working Home Page"})
 })
 
