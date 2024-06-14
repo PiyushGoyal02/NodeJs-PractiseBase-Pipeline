9c45252ca5fe0bafae73a362e595bda97795d5ef
PiyushGoyal02 <goyalp3542@gmail.com>
2024-06-14 22:33:01 +0530

    Name Add

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 
 app.get('/api', (req, res) => {
-    res.send({message: "Hey I am working Home Page"})
+    res.send({message: "Hey I am working Piyush Goyal"})
 })
 
 app.listen(PORT, () => console.log(`I Am Running PORT On ${PORT}`));
